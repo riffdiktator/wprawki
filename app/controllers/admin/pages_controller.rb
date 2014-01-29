98e53fd4d620a34645359432ecd48e7a146a8db1
Tomasz Lipiński <developer@riffdiktator.com>
2014-01-29 22:18:22 +0100

    Edycja podstron

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -14,6 +14,10 @@ class Admin::PagesController < ApplicationController
 
 	def edit
 		@numer = params[:id]
+		@tytul = Page.find(params[:id]).title
+		@tekst = Page.find(params[:id]).text
+		@utworzone = Page.find(params[:id]).created_at
+		@zmienione = Page.find(params[:id]).updated_at
 	end
 
 	def create 
@@ -21,7 +25,11 @@ class Admin::PagesController < ApplicationController
 	    @page.save
 	    redirect_to admin_pages_url
 	end
-
+	def update
+		@page = Page.find(params[:id])
+		@page.update(page_params)
+		redirect_to admin_pages_url
+	end
 	def destroy
 		@numer = params[:id]
 		Page.destroy(params[:id])
